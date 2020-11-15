00d6b81ddbfe749e1926c2473c468b52251273ff
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2020-11-15 17:13:07 +0100

    added cli

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Simple bot that keeps synchronized a blog wiht rss feed and a twitter account.
 
 All [twitter-developer](https://developer.twitter.com) credentials are enviroment variables stored in `.env` file.  
 
-```sh
+``` sh
 RSS=
 USER_ID=
 CONSUMER_KEY=
@@ -13,13 +13,22 @@ ACCESS_TOKEN=
 ACCESS_TOKEN_SECRET=
 ```
 
+or via cli 
+
+``` 
+twitter-bot 
+    --access-token <access-token> \
+    --access-token-secret <access-token-secret> \
+    --bearer-token <bearer-token> \
+    --consumer-key <consumer-key> \
+    --consumer-secret <consumer-secret> \
+    --rss <rss> \
+    --user-id <user-id>
+```
+
 `RSS` is the url of the blog feed
 
 ## Author
+
 **twitter-bot** © [gabelluardo](https://github.com/gabelluardo)  
 Released under the [MIT](https://github.com/gabelluardo/twitter-bot/blob/master/LICENSE) License.
-
-
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,79 @@
 use chrono::prelude::*;
-use dotenv;
+use dotenv::dotenv;
 use egg_mode::{auth, tweet, KeyPair, Token};
 use rss::Channel;
+use structopt::StructOpt;
 
 use std::time::Duration;
 
 type Date = DateTime<FixedOffset>;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(
+    name = "twitter-bot",
+    about = "A good bot that tweets automatically for every new blog post"
+)]
+struct Args {
+    #[structopt(long)]
+    user_id: Option<String>,
+
+    #[structopt(long)]
+    rss: Option<String>,
+
+    #[structopt(long)]
+    consumer_key: Option<String>,
+
+    #[structopt(long)]
+    consumer_secret: Option<String>,
+
+    #[structopt(long)]
+    access_token: Option<String>,
+
+    #[structopt(long)]
+    access_token_secret: Option<String>,
+
+    #[structopt(long)]
+    bearer_token: Option<String>,
+
+    /// Enable a minimal logging
+    #[structopt(short, long)]
+    log: bool,
+}
+
+impl Args {
+    fn parse() -> Self {
+        let args = Self::default();
+        match args.rss.is_none()
+            || args.user_id.is_none()
+            || args.consumer_key.is_none()
+            || args.consumer_secret.is_none()
+            || args.access_token.is_none()
+            || args.access_token_secret.is_none()
+            || args.bearer_token.is_none()
+        {
+            true => Self::from_args(),
+            _ => args,
+        }
+    }
+}
+
+impl Default for Args {
+    fn default() -> Self {
+        dotenv().ok();
+
+        Self {
+            user_id: dotenv::var("USER_ID").ok(),
+            rss: dotenv::var("RSS").ok(),
+            consumer_key: dotenv::var("CONSUMER_KEY").ok(),
+            consumer_secret: dotenv::var("CONSUMER_SECRET").ok(),
+            access_token: dotenv::var("ACCESS_TOKEN").ok(),
+            access_token_secret: dotenv::var("ACCESS_TOKEN_SECRET").ok(),
+            bearer_token: dotenv::var("BEARER_TOKEN").ok(),
+            log: false,
+        }
+    }
+}
 
 struct Post {
     title: String,
@@ -16,22 +83,26 @@ struct Post {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    dotenv::dotenv().ok();
+    let args = Args::parse();
+    dbg!(&args);
+    let token = get_token(args.clone()).await?;
+    verify_tokens(&token, args.log).await?;
 
-    let token = get_token().await?;
-    verify_tokens(&token, false).await?;
+    match (&args.rss, &args.user_id) {
+        (Some(rss), Some(user_id)) => {
+            loop {
+                let post = last_post(rss).await?;
+                let last_tweet_date = last_tweet(&token, user_id).await?;
 
-    loop {
-        let post = last_post(&dotenv::var("RSS")?).await?;
-        let last_tweet_date = last_tweet(&token).await?;
-
-        // check if last blog post is newer than last tweet
-        if post.date > last_tweet_date {
-            publish(&token, &post, false).await?;
+                // check if last blog post is newer than last tweet
+                if post.date > last_tweet_date {
+                    publish(&token, &post, false).await?;
+                }
+                // sleep for 2 hours
+                tokio::time::delay_for(Duration::from_secs(2 * 60 * 60)).await
+            }
         }
-
-        // sleep for 2 hours
-        tokio::time::delay_for(Duration::from_secs(2 * 60 * 60)).await
+        _ => Ok(eprintln!("Unable to parse RSS and USER_ID")),
     }
 }
 
@@ -50,9 +121,8 @@ async fn last_post(rss: &str) -> Result<Post> {
     Ok(Post { title, url, date })
 }
 
-async fn last_tweet(t: &Token) -> Result<Date> {
-    let user_id = dotenv::var("USER_ID")?;
-    let tl = egg_mode::tweet::user_timeline(user_id, false, false, &t);
+async fn last_tweet(t: &Token, user_id: &str) -> Result<Date> {
+    let tl = egg_mode::tweet::user_timeline(user_id.to_string(), false, false, &t);
     let (_, feed) = tl.start().await?;
 
     let tweet = feed.response.first().unwrap();
@@ -71,14 +141,16 @@ async fn publish(t: &Token, post: &Post, log: bool) -> Result<()> {
     Ok(())
 }
 
-async fn get_token() -> Result<Token> {
+async fn get_token(args: Args) -> Result<Token> {
     let consumer = KeyPair::new(
-        dotenv::var("CONSUMER_KEY")?,
-        dotenv::var("CONSUMER_SECRET")?,
+        args.consumer_key.expect("Unable to parse CONSUMER_KEY"),
+        args.consumer_secret
+            .expect("Unable to parse CONSUMER_SECRET"),
     );
     let access = KeyPair::new(
-        dotenv::var("ACCESS_TOKEN")?,
-        dotenv::var("ACCESS_TOKEN_SECRET")?,
+        args.access_token.expect("Unable to parse ACCESS_TOKEN"),
+        args.access_token_secret
+            .expect("Unable to parse ACCESS_TOKEN_SECRET"),
     );
 
     Ok(Token::Access { consumer, access })
